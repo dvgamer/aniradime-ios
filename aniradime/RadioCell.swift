a50b82a62a5d3a33517727bd52b08dc4e5eae872
rakuishi <rakuishi@gmail.com>
2016-11-04 15:37:35 +0800

    Update RadioCell’s design

--- aniradime/RadioCell.swift
+++ aniradime/RadioCell.swift
@@ -14,14 +14,11 @@ class RadioCell: UITableViewCell {
     @IBOutlet weak var radioTitleLabel: UILabel!
     @IBOutlet weak var radioDescriptionLabel: UILabel!
     @IBOutlet weak var radioThumbnailImageView: UIImageView!
-    @IBOutlet weak var radioPublsihedAtLabel: UILabel!
     
     var radio: Radio!
     var formatter: DateFormatter!
 
     override func awakeFromNib() {
-        self.formatter = DateFormatter()
-        self.formatter.dateFormat = "HH:mm"
     }
 
     func setRadio(_ radio: Radio) {
@@ -32,12 +29,10 @@ class RadioCell: UITableViewCell {
         self.radioTitleLabel.sizeToFit()
         rect.size.height = self.radioTitleLabel.frame.height
         self.radioTitleLabel.frame = rect
-        
-        self.radioPublsihedAtLabel.text = self.formatter.string(from: self.radio.publishedAt!)
 
         if self.radio.radioStation != nil {
             let station: String! = self.radio.radioStation?.name
-            self.radioDescriptionLabel?.text = "配信元: " + station
+            self.radioDescriptionLabel?.text = station
         }
 
         // Remove an already appeared image, which is recycled by UITableView
