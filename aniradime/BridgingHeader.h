ff00654cb927d8931f50b2e81b6273419484b415
rakuishi <rakuishi@gmail.com>
2016-11-05 22:39:48 +0800

    Fix the project’s settings

--- aniradime/BridgingHeader.h
+++ aniradime/BridgingHeader.h
@@ -1,12 +1,7 @@
-//
-//  NendAd_Bridging_Header.h
-//  NendSwiftSample
-//
-//  Created by ADN on 2015/06/05.
-//  Copyright (c) 2015年 F@N Communications. All rights reserved.
-//
 
+// vendors/Nend
 #ifndef NendSwiftSample_NendAd_Bridging_Header_h
+
 #define NendSwiftSample_NendAd_Bridging_Header_h
 
 #import "NADView.h"                  // バナー広告用ヘッダー
