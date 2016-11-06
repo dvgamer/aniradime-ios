de4284a6fb858f5b6043102770435e5a4eb87ece
rakuishi <rakuishi@gmail.com>
2016-11-06 09:32:28 +0800

    Simplify BridgingHeader.h

--- aniradime/BridgingHeader.h
+++ aniradime/BridgingHeader.h
@@ -1,12 +1,9 @@
 
 // vendors/Nend
-#ifndef NendSwiftSample_NendAd_Bridging_Header_h
+#ifndef NendBridgingHeader
 
-#define NendSwiftSample_NendAd_Bridging_Header_h
+#define NendBridgingHeader
 
-#import "NADView.h"                  // バナー広告用ヘッダー
-#import "NADInterstitial.h"          // インタースティシャル広告用ヘッダー
-#import "NADNativeClient.h"          // ネイティブ広告用ヘッダー
-#import "NADNativeTableViewHelper.h" // ネイティブ広告用ヘッダー (UITableView用)
+#import "NADView.h"
 
 #endif
